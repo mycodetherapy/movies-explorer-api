9c7b58aa41b3dbcb19fa6ea5eb5f9f381718b2b9
Max Ovchinnikov <ovchinnicov132@gmail.com>
2022-03-23 16:17:46 +0300

    feat add logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const { validateRegisterBody } = require('./validation');
 const { login, createUser } = require('./controllers/users');
@@ -16,16 +17,19 @@ mongoose.connect('mongodb://localhost:27017/moviedb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
+
 app.post('/signin', validateRegisterBody, login);
 app.post('/signup', validateRegisterBody, createUser);
 app.use(auth);
-app.use('/movies', require('./routes/movies'));
 app.use('/users', require('./routes/users'));
+app.use('/movies', require('./routes/movies'));
 
 app.use((req, res, next) => {
   next(new NotFoundError('Маршрут не существует.'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsHandler);
 
