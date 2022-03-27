c78374dfbc1815ef39d552bc5d9ed43e2d91a8c5
Max Ovchinnikov <ovchinnicov132@gmail.com>
2022-03-28 00:19:37 +0300

    fix: update email

--- validation/validationUpdateUser.js
+++ validation/validationUpdateUser.js
@@ -2,11 +2,14 @@ const { celebrate, Joi } = require('celebrate');
 
 const validationUpdateUser = celebrate({
   body: {
-    name: Joi.string().min(2).max(30).required()
+    name: Joi.string().min(2).max(30)
       .messages({
         'string.min': 'Минимальная длина поля 2 символа.',
         'string.max': 'Максимальная длина поля 30 символов.',
-        'any.required': 'Поле обязательно для заполнения.',
+      }),
+    email: Joi.string().email()
+      .message({
+        'string.email': 'Поле должно содержать адрес электронной почты.',
       }),
   },
 });
