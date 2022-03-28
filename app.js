f1641b9346de2714e7e8c1d118689dc5266fc9aa
Max Ovchinnikov <ovchinnicov132@gmail.com>
2022-03-28 10:23:46 +0300

    fix: delete unused constants in app

--- app.js
+++ app.js
@@ -4,19 +4,13 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 const { errors } = require('celebrate');
-// const cors = require('cors');
 const helmet = require('helmet');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter } = require('./limiter');
-const auth = require('./middlewares/auth');
-const { validateRegisterBody, validateLoginBody } = require('./validation');
-const { login, createUser } = require('./controllers/users');
 const errorsHandler = require('./middlewares/error-handler');
-const { NotFoundError } = require('./errors');
 
 app.use(express.json());
-// app.use(cors);
 
 mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
